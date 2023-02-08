fb29887449d9342933d4af33ea08e80a369963a1
Julia Isaksson <112895681+Juliastarstrike@users.noreply.github.com>
2023-02-08 18:07:10 +0100

    json works

--- Are you digging it/Assets/Save_And_Load_Script.cs
+++ Are you digging it/Assets/Save_And_Load_Script.cs
@@ -1,8 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Text;
 
-
+    [Serializable] public class PlayerSaveData : MonoBehaviour
+{
+    public string Name;
+    public float ColorHUE;
+    public bool Hidden;
+    public Vector3 Position;
+}
 public class Save_And_Load_Script : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -13,7 +22,7 @@ public class Save_And_Load_Script : MonoBehaviour
     void Start()
     {
         //blocks = GetComponent<"Score_manager">;
-        GameObject canvas = GameObject.Find("Canvas");
+        
         Score_manager score_manager = canvas.GetComponent<Score_manager>();
         score_manager.score = 0;
     }
@@ -25,9 +34,29 @@ public class Save_And_Load_Script : MonoBehaviour
     }
     public void Save()
     {
+        Debug.Log("funkar detta?");
+        PlayerSaveData saveData = new PlayerSaveData();
         GameObject canvas = GameObject.Find("Canvas");
         Score_manager score_manager = canvas.GetComponent<Score_manager>();
-        PlayerPrefs.SetInt("Destroyd_blocks", score_manager.score);
+        //PlayerPrefs.SetInt("Destroyd_blocks", score_manager.score);
+    
+
+        //Create our saveData object
+        
+        Debug.Log("hur går det?");
+        //Put our data in our object
+        saveData.Name = "Alva";
+        saveData.ColorHUE = 0.5f;
+        saveData.Hidden = false;
+        saveData.Position = transform.position;
+
+        //Convert saveData object to JSON
+        string jsonString = JsonUtility.ToJson(saveData);
+
+        //For now just save it using PlayerPrefs
+        PlayerPrefs.SetString("PlayerSaveData", jsonString);
+        SaveToFile("pelle.json",jsonString);
+        Debug.Log("hej hop");
     }
     public void Load()
     {
@@ -35,6 +64,29 @@ public class Save_And_Load_Script : MonoBehaviour
         GameObject canvas = GameObject.Find("Canvas");
         Score_manager score_manager = canvas.GetComponent<Score_manager>();
         score_manager.score = PlayerPrefs.GetInt("Destroyd_blocks");
-        
+    }
+
+    public void SaveToFile(string fileName, string jsonString)
+{
+    // Open a file in write mode. This will create the file if it's missing.
+    // It is assumed that the path already exists.
+    using (var stream = File.OpenWrite(fileName))
+    {
+        // Truncate the file if it exists (we want to overwrite the file)
+        stream.SetLength(0);
+
+        // Convert the string into bytes. Assume that the character-encoding is UTF8.
+        // Do you not know what encoding you have? Then you have UTF-8
+        var bytes = Encoding.UTF8.GetBytes(jsonString);
+
+        // Write the bytes to the hard-drive
+        stream.Write(bytes, 0, bytes.Length);
+
+        // The "using" statement will automatically close the stream after we leave
+        // the scope - this is VERY important
     }
 }
+
+}
+
+
